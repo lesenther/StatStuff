18225f4472477fec031355a6693144de402084ab
Logan Esenther <logan@cal.berkeley.edu>
2019-09-07 02:53:04 -0700

    Fixed scaling issue, added better support for granularity with scaled values

--- modules/display/index.js
+++ modules/display/index.js
@@ -8,8 +8,100 @@ const fadeBrick2 = '▒';
 const fadeBrick3 = '░';
 const leftBrick = '▌';
 const rightBrick = '▐';
+const emptyBrick = ' ';
 
 module.exports = _this => {
+
+  /**
+   *
+   *
+   * @param {*} x The index of the bin
+   * @param {*} y The middle value of the current line we are rendering
+   */
+  const _getBarSegment = (x, y, options) => {
+    const bucketIds = _this.getIds();
+    const currentBucketSize = _this.getById(bucketIds[x]).getSize();
+    const bucketWidth = _this.getMaxIdLength();
+    const yIncrement = 1 / options.scale || 1;
+    const lineNumber = Math.ceil(y / yIncrement);
+    const averageMarkChar = options.markerChar || fadeBrick3;
+    const meanBucketIndex = _this.getIds().map(id => _this.getAverage() - id).filter(id => id >= 0).length - 1;
+
+    const yCeil = y + yIncrement / 2;
+    const yFloor = y - yIncrement / 2;
+    const slope = _getSlope(x);
+    const leftChars = (bucketWidth > 1) ? Math.floor(bucketWidth / 2) : false;
+    const midChar = (bucketWidth % 2 !== 0);
+    const rightChars = bucketWidth - +midChar - +leftChars;
+
+    let chars = ``;
+
+    if (currentBucketSize >= yCeil) {
+      chars = fullBrick.repeat(bucketWidth);
+    } else if (currentBucketSize === y) {
+      chars = halfBrick.repeat(bucketWidth);
+    } else if (currentBucketSize < yCeil && currentBucketSize > yFloor) { // the bucket is within the current line window
+      const spacesAvailable = bucketWidth * 3;
+      const fillRatio = Math.floor((currentBucketSize - yFloor) / yIncrement * 10) / 10; // 0, 0.5, 1
+
+      if (fillRatio > 0.5) { // 0.5 to 1
+        if (bucketWidth > 2) {
+          chars = halfBrick.repeat(1) + fullBrick.repeat(bucketWidth - 2) + halfBrick.repeat(1);
+        } else {
+          chars = fullBrick.repeat(bucketWidth);
+        }
+      } else {
+        if (fillRatio < 0.1) { // 0 to 0.1
+          chars = miniBrick.repeat(bucketWidth);
+        } else { // 0.1 to 0.5
+          if (bucketWidth > 2) {
+            chars = miniBrick.repeat(1) + halfBrick.repeat(bucketWidth - 2) + miniBrick.repeat(1);
+          } else {
+            chars = halfBrick.repeat(bucketWidth);
+          }
+        }
+      }
+    } else {
+      return false;
+    }
+
+    // Use a special character around the base of the mean (will only appear if the mean bucket has items)
+    if (options.markAverage && _this.isNumeric() && lineNumber === 1 && x === meanBucketIndex) {
+      const meanPercent = (_this.getAverage() - bucketIds[x]) / (bucketIds[x + 1] - bucketIds[x]);
+      const charMark = meanPercent ? Math.round(meanPercent * bucketWidth) : 1;
+      chars = chars.split('');
+      chars[charMark - 1] = averageMarkChar || charMark === 1 ? leftBrick : rightBrick;
+      chars = chars.join('');
+    }
+
+    return chars;
+  }
+
+  /**
+   * Calculate the slope - used to feather the edges of the top of the histogram to add better resolution
+   *
+   * @param {*} index
+   * @param {*} bucketIds
+   */
+  const _getSlope = index => {
+    const buckets = _this.getIds();
+    const _leftSlope = _ => (buckets[index].getSize() - buckets[index - 1].getSize()) / (buckets[index].getId() - buckets[index - 1].getId());
+    const _rightSlope = _ => (buckets[index + 1].getSize() - buckets[index].getSize()) / (buckets[index + 1].getId() - buckets[index].getId());
+
+    if (buckets.length >= 1) {
+      return 0;
+    } else if (index === 0) { // Slope will be a number from -Inf to Inf
+      return _rightSlope();
+    } else if (index === buckets.length - 1) { // Slope will be a number from -Inf to Inf
+      return _leftSlope();
+    } else { // Slope will be an array of two numbers from -Inf to Inf
+      // Possible situations:  // /- /\ -/ -- -\ \/ \- \\   =>  3^2
+      return [
+        _leftSlope(),
+        _rightSlope()
+      ];
+    }
+  }
 
   /**
    * Get an array containing strings that visually depict the collection 
@@ -18,13 +110,13 @@ module.exports = _this => {
    * @param {object} options 
    */
   const _printVertical = (options = {}) => {
-    const yIncrement = 1 / options.scaleY || 1;
+    const yIncrement = 1 / options.scale || 1;
     const bucketIds = _this.getIds();
     const maxBucketSize = _this.getMaxSize() > 10
       ? Math.ceil(_this.getMaxSize() / 5) * 5
       : _this.getMaxSize();
     const maxBucketSizeLen = maxBucketSize.toString().length;
-    const averageMarkChar = options.markerChar || fadeBrick3;
+
     const usedYAxisVals = [];
     const output = [];
     const line = [];
@@ -43,56 +135,8 @@ module.exports = _this => {
         : (' '.repeat(maxBucketSizeLen) + ' │')) + sep);
 
       for (let x = 0; x < bucketIds.length; x++) {
-        const currentBucketSize = _this.getById(bucketIds[x]).getSize();
-        const bucketWidth = _this.getMaxIdLength();
-        const yCeil = y + yIncrement / 2;
-        const yFloor = y - yIncrement / 2;
-
-        let chars = '';
-
-        if (currentBucketSize >= yCeil) { // The bucket is bigger than the ceiling, fill it all
-          chars = fullBrick.repeat(bucketWidth);
-        } else if (currentBucketSize === y) { // exact match
-          chars = halfBrick.repeat(bucketWidth);
-        } else if (currentBucketSize < yCeil && currentBucketSize > yFloor) { // the bucket is within the current line window
-          const fillRatio = (currentBucketSize - yFloor) / yIncrement; // 0, 0.5, 1
-
-          if (fillRatio > 0.5) { // 0.5 to 1
-            if (bucketWidth > 2) {
-              chars = halfBrick.repeat(1) + fullBrick.repeat(bucketWidth - 2) + halfBrick.repeat(1);
-            } else {
-              chars = fullBrick.repeat(bucketWidth);
-            }
-          } else {
-            if (fillRatio < 0.1) { // 0 to 0.1
-              chars = miniBrick.repeat(bucketWidth);
-            } else { // 0.1 to 0.5
-              if (bucketWidth > 2) {
-                chars = miniBrick.repeat(1) + halfBrick.repeat(bucketWidth - 2) + miniBrick.repeat(1);
-              } else {
-                chars = halfBrick.repeat(bucketWidth);
-              }
-            }
-          }
-        } else {
-          chars = sep.repeat(_this.getMaxIdLength());
-        }
-
-        // Use a special character around the base of the mean (will only appear if the mean bucket has items)
-        if (options.markAverage && _this.isNumeric()) {
-          const mean = _this.getAverage();
-          const meanBucketIndex = _this.getIds().map(id => mean - id).filter(id => id >= 0).length - 1;
-
-          if (lineNumber === 1 && x === meanBucketIndex) {
-            const meanPercent = (mean - bucketIds[x]) / (bucketIds[x + 1] - bucketIds[x]);
-            const charMark = meanPercent ? Math.round(meanPercent * bucketWidth) : 1;
-            chars = chars.split('');
-            chars[charMark - 1] = averageMarkChar || charMark === 1 ? leftBrick : rightBrick;
-            chars = chars.join('');
-          }
-        }
-
-        line.push(chars + sep);
+        const barSegment = _getBarSegment(x, y, options) || sep.repeat(_this.getMaxIdLength());
+        line.push(barSegment + sep);
       }
 
       output.push(line.join(''));
@@ -117,13 +161,14 @@ module.exports = _this => {
    */
   const _printHorizontal = options => {
     options = extend({ // defaults
+      scale: 1
     }, options);
 
     return _this.getContainer().map(bucket => {
       const id = bucket.getId();
       const size = bucket.getSize();
       const padding = ' '.repeat(_this.getMaxIdLength() - id.length);
-      const bar = fullBrick.repeat(size);
+      const bar = fullBrick.repeat(Math.floor(size * options.scale));
 
       return `${padding}${id} │${bar} ${size}`;
     });
